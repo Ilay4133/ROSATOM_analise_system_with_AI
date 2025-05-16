1e216acfec167808692508709cd76414c120c701
Ilay_(Sleepy Mita) <148774440+Ilay4133@users.noreply.github.com>
2025-05-16 15:34:46 +0700

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,11 @@
 import sqlite3
 from datetime import datetime
 import pytz
-from backend.ai_model import model_start
+from rosatom_project.backend.ai_model import model_start1
 
 
 
-def create_db_table(db_path='C:/Users/artem/PycharmProjects/PythonProject/backend/filters.db'):
+def create_db_table1(db_path='C:/Users/User/PycharmProjects/pythonProject5/rosatom_project/backend/filters1.db'):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS filter_data
@@ -20,7 +20,7 @@ def create_db_table(db_path='C:/Users/artem/PycharmProjects/PythonProject/backen
     conn.close()
 
 
-def save_to_db(data, db_path='C:/Users/artem/PycharmProjects/PythonProject/backend/filters.db') -> list:
+def save_to_db1(data, db_path='C:/Users/User/PycharmProjects/pythonProject5/rosatom_project/backend/filters1.db') -> list:
     filter_status: str =""
     timestamp = datetime.now(pytz.timezone('Europe/Kaliningrad')).strftime('%Y-%m-%d %H:%M:%S')
     if data[1]==0:
@@ -45,18 +45,18 @@ def save_to_db(data, db_path='C:/Users/artem/PycharmProjects/PythonProject/backe
     print("Данные успешно сохранены в базу данных")
     return [data[1],int_filter_status, data[2],data[3], timestamp]
 
-def save_to_database() -> list:
-    create_db_table()
-    data_list_db=model_start()
+def save_to_database1() -> list:
+    create_db_table1()
+    data_list_db=model_start1()
     data = [
         1739127850170,
         data_list_db[0],
         data_list_db[1],
         data_list_db[2]
     ]
-    filter_values=save_to_db(data)
+    filter_values=save_to_db1(data)
 
-    conn = sqlite3.connect('C:/Users/artem/PycharmProjects/PythonProject/backend/filters.db')
+    conn = sqlite3.connect('C:/Users/User/PycharmProjects/pythonProject5/rosatom_project/backend/filters1.db')
     c = conn.cursor()
     c.execute("SELECT * FROM filter_data")
     print("\nСодержимое базы данных:")
@@ -64,5 +64,9 @@ def save_to_database() -> list:
         print(row)
     conn.close()
     return filter_values
+
+#_____________________________________________________________________________________________________
+#_____________________________________________________________________________________________________
+#_____________________________________________________________________________________________________
 
 
